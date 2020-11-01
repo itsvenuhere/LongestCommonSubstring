9de9e9250ed07d70da43950ec5cafb9e2eb2efb6
itsvenuhere <itsvenuhere@gmail.com>
2020-11-01 17:57:14 -0500

    Update MissingPayloadException.java

--- src/main/java/com/comcast/LongestCommonSubstring/exception/MissingPayloadException.java
+++ src/main/java/com/comcast/LongestCommonSubstring/exception/MissingPayloadException.java
@@ -3,6 +3,10 @@ package com.comcast.LongestCommonSubstring.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+* Return 400 Bad request when setOfStrings doesn't exists in request payload. Since the request payload schema doesn't match returning 400.
+*  TODO: Create new exception with status code 422, if request schema matches and can't process.
+*/
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "SetOfStrings must present with atleast one value")
 public class MissingPayloadException extends LCSException {
     public MissingPayloadException(String message) {
